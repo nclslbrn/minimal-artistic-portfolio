7089d804d3eede422845c887868f0700b07845d8
nclslbrn <nl@artemg.com>
2021-02-26 21:58:41 +0100

    add selfhosted option to plyr video player

--- inc/meta-box.php
+++ inc/meta-box.php
@@ -1,131 +1,130 @@
 <?php
 /*----------------BACKEND----------------*/
-function register_new_metabox( ) {
-    
+function register_new_metabox()
+{
     $event_meta_key = array( 'BEGINDATE', 'ENDDATE', 'PLACE', 'LATT', 'LONG' );
 
-    for( $m = 0; $m < count( $event_meta_key); $m++ ) {
-
+    for ($m = 0; $m < count($event_meta_key); $m++) {
         register_meta(
-            'post', 
+            'post',
             $event_meta_key[$m],
             array(
               'show_in_rest' => true,
               'type' => 'string',
               'single' => true,
               'sanitize_callback' => 'sanitize_text_field',
-              'auth_callback' => function() { 
+              'auth_callback' => function () {
                   return current_user_can('edit_posts');
               }
-        ));
-    
+        )
+        );
     }
-    
 }
 add_action('init', 'register_new_metabox');
 
 
-add_action( 'add_meta_boxes', 'adding_new_metabox' );
+add_action('add_meta_boxes', 'adding_new_metabox');
 
-function adding_new_metabox( ) {
+function adding_new_metabox()
+{
 
   // event
 
-  add_meta_box(
-    'date_section',
-    'Date',
-    'date_function',
-    'event',
-    'normal',
-    'high',
-    array(
+    add_meta_box(
+        'date_section',
+        'Date',
+        'date_function',
+        'event',
+        'normal',
+        'high',
+        array(
       '__back_compat_meta_box' => true
     )
-  );
+    );
 
-  add_meta_box(
-    'my-map',
-    'Google Map',
-    'map_iframe',
-    'event',
-    'normal',
-    'high',
-    array(
+    add_meta_box(
+        'my-map',
+        'Google Map',
+        'map_iframe',
+        'event',
+        'normal',
+        'high',
+        array(
       '__back_compat_meta_box' => true
     )
-  );
+    );
 
-  // projects
-   add_meta_box(
-    'cartel_section',
-    'Cartel',
-    'cartel_function',
-    'project',
-    'normal',
-    'high'
-  );
+    // projects
+    add_meta_box(
+        'cartel_section',
+        'Cartel',
+        'cartel_function',
+        'project',
+        'normal',
+        'high'
+    );
 
-  add_meta_box(
-    'event',
-    __('Event', 'Minimal-Artistic-Portfolio'),
-    'event_link',
-    'project',
-    'side',
-    'high',
-    array(
+    add_meta_box(
+        'event',
+        __('Event', 'Minimal-Artistic-Portfolio'),
+        'event_link',
+        'project',
+        'side',
+        'high',
+        array(
       '__back_compat_meta_box' => true
     )
-  );
+    );
 
-  add_meta_box(
-    'cover_options',
-    __('Cover option', 'Minimal-Artistic-Portfolio'),
-    'cover_options',
-    'project',
-    'side',
-    'high',
-    array(
+    add_meta_box(
+        'cover_options',
+        __('Cover option', 'Minimal-Artistic-Portfolio'),
+        'cover_options',
+        'project',
+        'normal',
+        'high',
+        array(
       '__back_compat_meta_box' => true
     )
-  );
+    );
 
-  /*
-  // pages
-  add_meta_box(
-    'page-section',
-    'Page Section',
-    'page_section',
-    'page',
-    'normal',
-    'high'
-  );
-  */
+    /*
+    // pages
+    add_meta_box(
+      'page-section',
+      'Page Section',
+      'page_section',
+      'page',
+      'normal',
+      'high'
+    );
+    */
 }
 
-function cartel_function( $post ) {
-  //so, dont ned to use esc_attr in front of get_post_meta
-  $cartel=  get_post_meta( $post->ID, 'CARTEL' , true ) ;
-  wp_editor( 
-    htmlspecialchars_decode($cartel), 
-    'mettaabox_ID_stylee', 
-    $settings = array('textarea_name'=>'Cartel')
-  );
+function cartel_function($post)
+{
+    //so, dont ned to use esc_attr in front of get_post_meta
+    $cartel=  get_post_meta($post->ID, 'CARTEL', true) ;
+    wp_editor(
+        htmlspecialchars_decode($cartel),
+        'mettaabox_ID_stylee',
+        $settings = array('textarea_name'=>'Cartel')
+    );
 }
-function date_function( $post ) {
-
-    $beginDate =  get_post_meta( $post->ID, 'BEGINDATE' , true );
-    $endDate =  get_post_meta( $post->ID, 'ENDDATE' , true );
-    ?>
+function date_function($post)
+{
+    $beginDate =  get_post_meta($post->ID, 'BEGINDATE', true);
+    $endDate =  get_post_meta($post->ID, 'ENDDATE', true); ?>
     <input type="date" name="InputBeginDate" id="beginDate" value="<?= $beginDate ?>">
     <input type="date" name="InputEndDate" id="endDate" value="<?= $endDate ?>">
     <?php
 }
 
-function event_link() {
-
+function event_link()
+{
     global $post;
-    $custom  = get_post_custom( $post->ID );
-    $savedEventId  = get_post_meta( $post->ID, 'event', true);
+    $custom  = get_post_custom($post->ID);
+    $savedEventId  = get_post_meta($post->ID, 'event', true);
     $count   = 0;
     echo '<div class="link_header">';
     $query_event_args = array(
@@ -133,22 +132,17 @@ function event_link() {
             'posts_per_page' => -1,
             );
 
-    $query_event = new WP_Query( $query_event_args );
+    $query_event = new WP_Query($query_event_args);
     $events = array();
 
-    foreach ( $query_event->posts as $event) {
-
+    foreach ($query_event->posts as $event) {
         $id = $event->ID;
-        $title = apply_filters( 'the_title', $event->post_title );
+        $title = apply_filters('the_title', $event->post_title);
 
-       if(is_array($savedEventId) && in_array($id, $savedEventId)){
-
-          echo '<input type="checkbox" name="event[]" value="'.$id.'" checked>'.$title.'<br>';
-
+        if (is_array($savedEventId) && in_array($id, $savedEventId)) {
+            echo '<input type="checkbox" name="event[]" value="'.$id.'" checked>'.$title.'<br>';
         } else {
-
-          echo '<input type="checkbox" name="event[]" value="'.$id.'">'.$title.'<br>';
-
+            echo '<input type="checkbox" name="event[]" value="'.$id.'">'.$title.'<br>';
         }
         $count++;
     }
@@ -156,13 +150,11 @@ function event_link() {
     echo '<div class="event_count"><span>Total:</span> <b>'.$count.'</b></div>';
 }
 
-function map_iframe( $post ) {
-
-    $place = get_post_meta( $post->ID, 'PLACE' , true );
-    $latt = get_post_meta( $post->ID, 'LATT' , true );
-    $long = get_post_meta( $post->ID, 'LONG' , true );
-
-    ?>
+function map_iframe($post)
+{
+    $place = get_post_meta($post->ID, 'PLACE', true);
+    $latt = get_post_meta($post->ID, 'LATT', true);
+    $long = get_post_meta($post->ID, 'LONG', true); ?>
     <label> <?php _e('Place', 'Minimal-Artistic-Portfolio'); ?></label>
     <input type="text" name="InputPlace" id="place" value="<?= $place ?>"/>
     <label>Lattitude</label>
@@ -172,19 +164,21 @@ function map_iframe( $post ) {
     <?php
 }
 
-function cover_options( $post ) {
-  $is_video = get_post_meta( $post->ID, 'IS_VIDEO', true); // o = image 1 = video
-  $video_id = get_post_meta( $post->ID, 'VIDEO_ID', true);
-  $video_provider = get_post_meta( $post->ID, 'VIDEO_PROVIDER', true);
-
-  ?>
+function cover_options($post)
+{
+    $is_video = get_post_meta($post->ID, 'IS_VIDEO', true); // o = image 1 = video
+    $video_id = get_post_meta($post->ID, 'VIDEO_ID', true);
+    $video_provider = get_post_meta($post->ID, 'VIDEO_PROVIDER', true);
+    $firstResUrl = get_post_meta($post->ID, '576P_VIDEO_URL', true);
+    $secondResUrl = get_post_meta($post->ID, '720P_VIDEO_URL', true);
+    $thirdResUrl = get_post_meta($post->ID, '1080P_VIDEO_URL', true); ?>
   <label><?php _e('Cover is an image or a video ?', 'Minimal-Artistic-Portfolio'); ?></label><br/>
   <input id="not_a_video" type="radio" name="is_video" value="0" <?php echo $is_video == '0' ? ' checked' : ''; ?>/>
   <label for="not_a_video"><?php _e('Image', 'Minimal-Artistic-Portfolio'); ?></label> <br/>
   <input id="its_a_video" type="radio" name="is_video" value="1" <?php echo $is_video == '1' ? ' checked' : ''; ?>/>
   <label><?php _e('Video', 'Minimal-Artistic-Portfolio'); ?></label><br/>
 
-  <label><?php _e('Video ID', 'Minimal-Artistic-Portfolio'); ?></label><br/>
+  <label><?php _e('Video ID (leave empty if selfhosted)', 'Minimal-Artistic-Portfolio'); ?></label><br/>
   <input type="text" name="video_id" value="<?php echo $video_id; ?>" /><br/>
 
   <label><?php _e('Video provider', 'Minimal-Artistic-Portfolio'); ?></label><br/>
@@ -192,6 +186,21 @@ function cover_options( $post ) {
   <label for="provider_vimeo"><?php _e('Vimeo', 'Minimal-Artistic-Portfolio'); ?></label><br/>
   <input id="provider_youtube" type="radio" name="video_provider" value="youtube" <?php echo $video_provider == 'youtube' ? ' checked' : ''; ?>/>
   <label for="provider_youtube"><?php _e('YouTube', 'Minimal-Artistic-Portfolio'); ?></label><br/>
+  <input id="provider_youtube" type="radio" name="video_provider" value="self" <?php echo $video_provider == 'self' ? ' checked' : ''; ?>/>
+  <label for="provider_youtube"><?php _e('Self hosted', 'Minimal-Artistic-Portfolio'); ?></label><br/>
+
+  <label><?php _e('Self hosting URLs', 'Minimal-Artistic-Portfolio'); ?></label><br/>
+  <label for="576pVideoUrl">576p video URL</label><br/>
+  <input id="576pVideoUrl" type="url" name="576pVideoUrl" value="<?php echo isset($firstResUrl) ? $firstResUrl : ''; ?>">
+  <br/>
+  
+  <label for="720pVideoUrl">720p video URL</label><br/>
+  <input id="720pVideoUrl" type="url" name="720pVideoUrl" value="<?php echo isset($secondResUrl) ? $secondResUrl : ''; ?>">
+  <br/>
+
+  <label for="1080pVideoUrl">1080p video URL</label><br/>
+  <input id="1080pVideoUrl" type="url" name="1080pVideoUrl" value="<?php echo isset($thirdResUrl) ? $thirdResUrl : ''; ?>">
+  <br/>
 
   <?php
 }
@@ -220,7 +229,7 @@ function page_section( $post ) {
   <ul id="sections-data-fields">
     <?php if ( $sections_data ) : ?>
 
-    	<?php foreach ( $sections_data as $i => $section) : ?>
+        <?php foreach ( $sections_data as $i => $section) : ?>
         <li class="active-section">
           <label for="title<? $i ?>]">
             <?php _e('Section title','Minimal-Artistic-Portfolio'); ?>
@@ -247,7 +256,7 @@ function page_section( $post ) {
     <?php _e('Add section', 'Minimal-Artistic-Portfolio'); ?>
   </button>
   <script type="text/javascript">
-	  // <![CDATA[
+      // <![CDATA[
     jQuery(document).ready(function( $ ){
 
       $( '#add-section' ).on('click', function(e) {
@@ -256,17 +265,17 @@ function page_section( $post ) {
 
           var i = $( 'ul#sections-data-fields li.active-section' ).length + 1;
 
-			 var titleEditorID = 'title' + i;
-			 var textEditorID = 'text' + i;
+             var titleEditorID = 'title' + i;
+             var textEditorID = 'text' + i;
 
           var sectionFields = '<li class="active-section">';
-			 sectionFields += '<label for="section_title[]"><?php _e('Section title','Minimal-Artistic-Portfolio'); ?></label>';
-			 sectionFields += '<textarea name="section_text[]" id="' + titleEditorID + '"></textarea>';
-			 sectionFields += '<label for="section_text[]"><?php _e('Section text','Minimal-Artistic-Portfolio'); ?></label>';
-			 sectionFields += '<textarea name="section_text[]" id="' + textEditorID + '"></textarea>';
-			 sectionFields += '</li>';
+             sectionFields += '<label for="section_title[]"><?php _e('Section title','Minimal-Artistic-Portfolio'); ?></label>';
+             sectionFields += '<textarea name="section_text[]" id="' + titleEditorID + '"></textarea>';
+             sectionFields += '<label for="section_text[]"><?php _e('Section text','Minimal-Artistic-Portfolio'); ?></label>';
+             sectionFields += '<textarea name="section_text[]" id="' + textEditorID + '"></textarea>';
+             sectionFields += '</li>';
 
-			 $("ul#sections-data-fields").append( sectionFields );
+             $("ul#sections-data-fields").append( sectionFields );
           tinymce.init({ selector: '#' + titleEditorID });
           tinymce.init({ selector: '#' + textEditorID });
 
@@ -280,81 +289,88 @@ function page_section( $post ) {
       });
 
     });
-  		// ]]>
-	 </script>
+        // ]]>
+     </script>
   <?php
 }
 */
-function save_my_postdata( $post_id ) {
-
-   if ( (!empty($_POST['InputBeginDate'])) && (!empty($_POST['InputEndDate'])) )  {
+function save_my_postdata($post_id)
+{
+    if ((!empty($_POST['InputBeginDate'])) && (!empty($_POST['InputEndDate']))) {
         $beginDateData=htmlspecialchars($_POST['InputBeginDate']);
         $endDateData=htmlspecialchars($_POST['InputEndDate']);
 
-        update_post_meta($post_id, 'BEGINDATE', $beginDateData );
-        update_post_meta($post_id, 'ENDDATE', $endDateData );
-   }
+        update_post_meta($post_id, 'BEGINDATE', $beginDateData);
+        update_post_meta($post_id, 'ENDDATE', $endDateData);
+    }
 
-   if ( (!empty($_POST['InputLatt'])) && (!empty($_POST['InputLong'])) ) {
+    if ((!empty($_POST['InputLatt'])) && (!empty($_POST['InputLong']))) {
         $cityMapData=htmlspecialchars($_POST['InputPlace']);
         $lattMapData=htmlspecialchars($_POST['InputLatt']);
         $longMapData=htmlspecialchars($_POST['InputLong']);
-        update_post_meta($post_id, 'PLACE', $cityMapData );
-        update_post_meta($post_id, 'LATT', $lattMapData );
-        update_post_meta($post_id, 'LONG', $longMapData );
-   }
+        update_post_meta($post_id, 'PLACE', $cityMapData);
+        update_post_meta($post_id, 'LATT', $lattMapData);
+        update_post_meta($post_id, 'LONG', $longMapData);
+    }
 
 
-   global $post;
+    global $post;
 
-   if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-       return $post->ID;
-   }
-
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+        return $post->ID;
+    }
 } // end save_custom_meta_data
-add_action( 'save_post_event', 'save_my_postdata' );
+add_action('save_post_event', 'save_my_postdata');
 
-function save_project( $post_id ) {
+function save_project($post_id)
+{
+    if (!empty($_POST['Cartel'])) {
+        $cartel = htmlspecialchars($_POST['Cartel']);
+        update_post_meta($post_id, 'CARTEL', $cartel);
+    }
+    if (!empty($_POST["event"])) {
+        //$update = implode(',', $_POST["event"]);
+        $events_id = $_POST["event"];
+        update_post_meta($post_id, "event", $events_id);
 
-  if (!empty($_POST['Cartel']))  {
-    $cartel = htmlspecialchars($_POST['Cartel']);
-    update_post_meta($post_id, 'CARTEL', $cartel );
-  }
-  if ( !empty( $_POST["event"] ) )  {
-    //$update = implode(',', $_POST["event"]);
-    $events_id = $_POST["event"];
-    update_post_meta( $post_id, "event", $events_id);
-
-    //wp_die( var_dump( $_POST["event"] )  );
-  }
-  if( isset( $_POST["is_video"]) && is_numeric( $_POST["is_video"] ) ) {
-    $is_video = htmlspecialchars($_POST['is_video']);
-    update_post_meta( $post_id, 'IS_VIDEO', $is_video);
-  }
+        //wp_die( var_dump( $_POST["event"] )  );
+    }
+    if (isset($_POST["is_video"]) && is_numeric($_POST["is_video"])) {
+        $is_video = htmlspecialchars($_POST['is_video']);
+        update_post_meta($post_id, 'IS_VIDEO', $is_video);
+    }
   
-  if( !empty( $_POST["video_id"]) ) {
-    $video_id = htmlspecialchars($_POST['video_id']);
-    update_post_meta( $post_id, "VIDEO_ID", $video_id );
-  }
-  
-  if( 
-    !empty( $_POST["video_provider"] ) 
-    && in_array( $_POST["video_provider"], array( 'vimeo', 'youtube') ) 
+    if ($_POST["video_id"] !== get_post_meta($post_id, "VIDEO_ID", true)) {
+        $video_id = htmlspecialchars($_POST['video_id']);
+        update_post_meta($post_id, "VIDEO_ID", $video_id);
+    }
+    if ($_POST["576pVideoUrl"] !== get_post_meta($post_id, "576P_VIDEO_URL", true)) {
+        update_post_meta($post_id, "576P_VIDEO_URL", htmlspecialchars($_POST["576pVideoUrl"]));
+    }
+    if ($_POST["720pVideoUrl"] !== get_post_meta($post_id, "720P_VIDEO_URL", true)) {
+        update_post_meta($post_id, "720P_VIDEO_URL", htmlspecialchars($_POST["720pVideoUrl"]));
+    }
+    if ($_POST["1080pVideoUrl"] !== get_post_meta($post_id, "1080P_VIDEO_URL", true)) {
+        update_post_meta($post_id, "1080P_VIDEO_URL", htmlspecialchars($_POST["1080pVideoUrl"]));
+    }
+
+    if (
+    !empty($_POST["video_provider"])
+    && in_array($_POST["video_provider"], array( 'vimeo', 'youtube', 'self'))
   ) {
-    
-    $video_provider = htmlspecialchars( $_POST["video_provider"] );
-    update_post_meta( $post_id, "VIDEO_PROVIDER", $video_provider );
-  }
+        $video_provider = htmlspecialchars($_POST["video_provider"]);
+        update_post_meta($post_id, "VIDEO_PROVIDER", $video_provider);
+    }
 }
 
-add_action( 'save_post_project', 'save_project' );
+add_action('save_post_project', 'save_project');
 /*
 function save_page_section( $post_id ) {
 
   if ( !isset( $_POST['section_data_nonce'] ) ) return;
   if ( !wp_verify_nonce( $_POST['section_data_nonce'], 'section_data_meta_box_nonce' ) ) {
-		return $post->ID;
-	}
+        return $post->ID;
+    }
   if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) return;
   if ( ! current_user_can( 'edit_page', $post_id ) ) return;
 
@@ -387,63 +403,62 @@ function save_page_section( $post_id ) {
 add_action( 'save_post', 'save_page_section');
 */
 
-function event_table_head( $defaults ) {
-  unset($defaults['categories']);
-  unset($defaults['date']);
-  $defaults['event_date']  = __('Date', 'Minimal-Artistic-Portfolio');
-  $defaults['place']   = __('Place', 'Minimal-Artistic-Portfolio');
-  return $defaults;
+function event_table_head($defaults)
+{
+    unset($defaults['categories']);
+    unset($defaults['date']);
+    $defaults['event_date']  = __('Date', 'Minimal-Artistic-Portfolio');
+    $defaults['place']   = __('Place', 'Minimal-Artistic-Portfolio');
+    return $defaults;
 }
 add_filter('manage_event_posts_columns', 'event_table_head');
 
 
-function event_table_content( $column_name, $post_id ) {
-
+function event_table_content($column_name, $post_id)
+{
     if ($column_name == 'event_date') {
-      echo  date_i18n('j F Y', strtotime(get_post_meta( $post_id, 'BEGINDATE', 'true') ) );
+        echo  date_i18n('j F Y', strtotime(get_post_meta($post_id, 'BEGINDATE', 'true')));
     }
 
     if ($column_name == 'place') {
-      $place = get_post_meta( $post_id, 'PLACE' , true );
-      echo $place;
+        $place = get_post_meta($post_id, 'PLACE', true);
+        echo $place;
     }
 }
-add_action( 'manage_event_posts_custom_column', 'event_table_content', 10, 2 );
+add_action('manage_event_posts_custom_column', 'event_table_content', 10, 2);
 
 
 /*----------------FRONTEND----------------*/
 
-function get_metabox($post_id, $key, $single) {
+function get_metabox($post_id, $key, $single)
+{
+    $meta_values = get_post_meta($post_id, $key, $single);
 
-    $meta_values = get_post_meta( $post_id, $key, $single );
-
-    if(!empty($meta_values)):
+    if (!empty($meta_values)):
         ?>
 
         <?php
-        if(!is_array($meta_values)) {
+        if (!is_array($meta_values)) {
             echo '<div class="' . strtolower($key) .'">';
             echo $meta_values;
             echo '</div>';
-
-        } elseif(is_array($meta_values)) {
+        } elseif (is_array($meta_values)) {
             echo '<div class="' . strtolower($key) .'">';
             $values ="";
-            foreach($meta_values as $value) {
+            foreach ($meta_values as $value) {
                 echo html_entity_decode($value);
             }
             echo '</div>';
         }
     endif;
 }
-function isset_metabox($post_id, $key, $single) {
-
-    $meta_values = get_post_meta( $post_id, $key, $single );
+function isset_metabox($post_id, $key, $single)
+{
+    $meta_values = get_post_meta($post_id, $key, $single);
 
     $data = array();
 
-    if(!empty($meta_values)){
-
+    if (!empty($meta_values)) {
         $data[$key] = $meta_values;
 
         //echo $key ." ". $data[$key] . "\n";
--- template-parts/content-project.php
+++ template-parts/content-project.php
@@ -1,10 +1,10 @@
-<?php if( !is_single() ) { ?>
+<?php if (!is_single()) { ?>
 
   <div id="project-<?php echo get_the_ID(); ?>" class="project-summary clearfix row">
 
     <div class="col-5 column">
       <a href="<?php echo get_permalink(); ?>" class="project-cover">
-        <?php echo get_the_post_thumbnail( get_the_ID(), 'cover'); ?>
+        <?php echo get_the_post_thumbnail(get_the_ID(), 'cover'); ?>
       </a>
     </div>
     <div class="col-7 column cartel">
@@ -17,7 +17,7 @@
         </h4>
 
         <p class="description">
-          <?php $cartel =  get_post_meta( get_the_ID(), 'CARTEL', true); ?>
+          <?php $cartel =  get_post_meta(get_the_ID(), 'CARTEL', true); ?>
           <?php echo $cartel; ?>,
           <span class="date">
             <?php echo mysql2date("Y.", $post->post_date_gmt); ?>
@@ -34,53 +34,72 @@
 
   </div>
 
-<?php } if( is_single() ) { 
-  
-  $is_video = get_post_meta( $post->ID, 'IS_VIDEO', true); // o = image 1 = video
-  $video_id = get_post_meta( $post->ID, 'VIDEO_ID', true);
-  $video_provider = get_post_meta( $post->ID, 'VIDEO_PROVIDER', true);
-  
-  ?>
+<?php } if (is_single()) {
+    $is_video = get_post_meta($post->ID, 'IS_VIDEO', true); // o = image 1 = video
+    $video_id = get_post_meta($post->ID, 'VIDEO_ID', true);
+    $video_provider = get_post_meta($post->ID, 'VIDEO_PROVIDER', true);
+    $firstResUrl = get_post_meta($post->ID, '576P_VIDEO_URL', true);
+    $secondResUrl = get_post_meta($post->ID, '720P_VIDEO_URL', true);
+    $thirdResUrl = get_post_meta($post->ID, '1080P_VIDEO_URL', true); ?>
   <header class="entry-header">
     <h1 class="project-title"><?php echo get_the_title(); ?></h1>
   </header>
 
 
   <div class="entry-content">
-  <?php if( 
-      $is_video == '1' 
-      && !empty( $video_id) 
-      && in_array( $video_provider, array( 'vimeo', 'youtube') ) ) {
-        
-        echo '<div class="player" 
-                   data-plyr-provider="'.$video_provider.'" 
-                   data-plyr-embed-id="'.$video_id.'">
+  <?php
+    if ($is_video == '1' && (
+        (isset($video_id) && in_array($video_provider, array( 'vimeo', 'youtube')))
+        ||
+        ($video_provider == 'self' && isset($thirdResUrl))
+    )
+    ) {
+        if (in_array($video_provider, array( 'vimeo', 'youtube'))) {
+            echo '<div class="player" 
+                data-plyr-provider="'.$video_provider.'" 
+                data-plyr-embed-id="'.$video_id.'">
               </div>';
-
-      } else  {
-        echo get_the_post_thumbnail( get_the_ID(), 'full');
-      }
-
-  ?>
+        } elseif ($video_provider == 'self' && isset($thirdResUrl)) {
+            echo '<video class="player selfhosted" controls crossorigin playsinline loop>
+                  '.(isset($firstResUrl) ? '<source src=\''.$firstResUrl.'\' type=\'video/mp4\' size=\'576\'> ' : '')
+                  .(isset($secondResUrl) ? '<source src=\''.$secondResUrl.'\' type=\'video/mp4\' size=\'720\'>' : '').'
+                  <source src=\''.$thirdResUrl.'\' type=\'video/mp4\' size=\'1080\'>
+                  <p>'.
+                    __('Your browser doesn\'t support HTML5 video, but you can read this video with the link below.', 'Minimal-Artistic-Portfolio')
+                    .'
+                    <a href=\''.$thirdResUrl.'\'>'.__('Read the video', 'Minimal-Artistic-Portfolio') .'</a>
+                    </p>
+                  <source src=\''.$thirdResUrl.'\' type=\'video/mp4\' size=\'1080\'>
+                </video>';
+        }
+    } else {
+        echo get_the_post_thumbnail(get_the_ID(), 'full');
+    } ?>
 
     <div class="row project-text
-    <?php if( get_the_content() !== '') echo ' filled'; ?>
-    <?php if( get_event(get_the_ID()) == false) echo ' no-event'; ?>">
+    <?php if (get_the_content() !== '') {
+        echo ' filled';
+    } ?>
+    <?php if (get_event(get_the_ID()) == false) {
+        echo ' no-event';
+    } ?>">
       <div class="col-4 column">
         <p class="cartel">
           <?php
-            $cartel =  get_post_meta( get_the_ID(), 'CARTEL', true);
-            echo $cartel;
-            ?>,
+            $cartel =  get_post_meta(get_the_ID(), 'CARTEL', true);
+    echo $cartel; ?>,
           <span class="date"><?php echo mysql2date("Y", $post->post_date_gmt); ?></span>.
         </p>
-        <?php if( get_event(get_the_ID()) !== false ) echo get_event( get_the_ID() ); ?>
-        <?php social_module( get_the_title(), get_the_permalink( get_the_ID() ), 'laptop' ); ?>
+        <?php if (get_event(get_the_ID()) !== false) {
+        echo get_event(get_the_ID());
+    } ?>
+        <?php social_module(get_the_title(), get_the_permalink(get_the_ID()), 'laptop'); ?>
       </div>
       <div class="col-8 column">
         <?php the_content(); ?>
       </div>
-		<?php social_module( get_the_title(), get_the_permalink( get_the_ID() ), 'mobile' ); ?>
+		<?php social_module(get_the_title(), get_the_permalink(get_the_ID()), 'mobile'); ?>
     </div>
   </div>
-<?php } ?>
+<?php
+} ?>
