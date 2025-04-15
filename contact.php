3700a1db313ae4ef9789182990679b12587763ef
nclslbrn <nclslbrn@gmail.com>
2025-04-15 21:06:58 +0200

    update font menu and background

--- contact.php
+++ contact.php
@@ -48,12 +48,6 @@ get_header(); ?>
 										</a>
 									</li>
 									<li>
-										<a href="https://deca.art/nclslbrn">
-											<img src="https://deca.art/icons/favicon-32x32.png" width="32" heigth="32"/>
-											<span>deca</span>
-										</a>
-									</li>
-									<li>
 										<a href="https://forsaken-ideas.nicolas-lebrun.fr/">
 											<img src="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📖</text></svg>" width="32" heigth="32">
 											<span>Forsaken Ideas</span>
